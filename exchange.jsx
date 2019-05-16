4ea3542589145fb991f5a381dc72f9dcdd0f6a09
seeo <aragonlotr5@gmail.com>
2019-05-16 15:29:23 +0800

    changed from index.html to exchange.html

--- exchange.jsx
+++ exchange.jsx
@@ -1,67 +1,93 @@
-console.log("all your scripts are belong to us");
-
-class Form extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-                        username: " ",
-                        password: " ",
-                    };
+class Exchanger extends React.Component{
+    constructor(){
+        super()
+        this.state={
+            sgd: "",
+            yen: "",
+            usd:"",
+            rmb:"",
+        }
     }
-
-    handleChange = e => {
-        this.setState({ [e.target.name]: e.target.value });
-        console.log(this.state);
-    };
-
+    inputMoney=(e)=>{
+        console.log(e.target.value);
+        this.setState({sgd: e.target.value})
+        this.convertMoney(e.target.value)
+    }
+    convertMoney=(giveMe)=>{
+        let sgdRaw = giveMe;
+        let yenNow = giveMe * 79.97;
+        let usdNow = giveMe * 0.73;
+        let rmbNow = giveMe * 5.02;
+        console.log('Japanese Yen: ', yenNow);
+        this.setState({yen: yenNow})
+        this.setState({usd: usdNow})
+        this.setState({rmb: rmbNow})
+    }
     render(){
-        return (
+        console.log('SGD STUFF', this.state.sgd);
+        return(
             <div>
-                <div>My Form</div>
-                <div onChange={this.handleChange}>
-                    <div>
-                        Name:{" "}
-                        <input
-                            name="username"
-                            type="text"
-                            id="inputUsername"
-                            placeholder="Enter username"
-                            defaultValue={this.state.username}
-                        />
-                    </div>
-                    <div>
-                        Password:{" "}
-                        <input
-                            name="password"
-                            type="password"
-                            id="inputPassword"
-                            placeholder="Enter password"
-                            defaultValue={this.state.password}
-                        />
-                    </div>
-                </div>
+                <Form
+                buttonMe={this.inputMoney}
+                placeholder = 'Input SGD AMOUNT'
+                type="number"/>
+                <YenConverted yenStuff={this.state.yen}/>
+                <UsdConverted usdStuff={this.state.usd}/>
+                <RmbConverted rmbStuff={this.state.rmb}/>
             </div>
         );
     }
 }
-
-class Exchanger extends React.Component{
-     constructor(props) {
-         super(props);
-         this.state = {
-             sgd: "",
-         }
-     }
+class Form extends React.Component{
     render(){
         return(
             <div>
-
+                <input placeholder = 'Input SGD AMOUNT'
+                onChange={(e)=>{this.props.buttonMe(e)}}/>
             </div>
-        )
+        );
     }
 }
-
-
+class UsdConverted extends React.Component{
+    render(){
+        console.log('inside Yen stuff', );
+        return(
+            <div>
+            Dollar Amerikana (USD) - {this.props.usdStuff}
+            </div>
+        );
+    }
+}
+class YenConverted extends React.Component{
+    render(){
+        console.log('inside Yen stuff', );
+        return(
+            <div>
+            Japanese Yen(JPY) - {this.props.yenStuff}
+            </div>
+        );
+    }
+}
+class RmbConverted extends React.Component{
+    render(){
+        console.log('inside Yen stuff', );
+        return(
+            <div>
+            Chinkos (RMB) - {this.props.rmbStuff}
+            </div>
+        );
+    }
+}
+class App extends React.Component{
+    render(){
+        return(
+            <div>
+                <Exchanger/>
+            </div>
+        );
+    }
+}
 ReactDOM.render(
-    <Form />,
-    document.getElementById("root"));
+    <App/>,
+    document.getElementById('root')
+);
